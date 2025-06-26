0aa30643f3ec1fc9f86f056f5aa1ba67b3a99bcb
petermainakihara <pkihara648@gmail.com>
2025-06-26 13:01:14 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here!// Remove the <main> element with id 'main'
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set the id of the new header to 'victory'
+newHeader.id = "victory";
+
+// Set the text content of the new header
+newHeader.textContent = "Peter Kihara is the champion";
+
+// Append the new header to the body
+document.body.append(newHeader);
